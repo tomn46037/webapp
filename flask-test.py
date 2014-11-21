799e1e39ee7db215a5b37a9754058bb7533a3527
Tom Neville <tomn@sneaky.net>
2014-11-21 02:36:28 +0000

    Some quick updates.. added another button.

--- flask-test.py
+++ flask-test.py
@@ -11,10 +11,12 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(17, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
 GPIO.setup(22, GPIO.OUT)
 
-GPIO.output(17, True);
-GPIO.output(22, True);
+GPIO.output(17, False);
+GPIO.output(21, False);
+GPIO.output(22, False);
 
 
 
